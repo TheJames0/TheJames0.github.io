1e9662b99d0b2e6927f59ab73db0e2c77d52566f
James Thomas Mather <101518239+TheJames0@users.noreply.github.com>
2026-06-15 16:58:28 +0100

    Update portfolio project pages and images

--- projectpopups.js
+++ projectpopups.js
@@ -1,8 +1,6 @@
 function navigateToProject(projectNumber) {
-    // Assuming you have a modal with the id 'myModal'
     $('#myModal').modal('show');
 
-    // Set the content of the modal based on the project number
     var projectContent = 'project' + projectNumber + '.html';
     console.log(projectContent);
     $.ajax({
